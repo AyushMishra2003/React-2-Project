0bbfe2237bb7324ca1f47a303a8d6944e7ae0c8f
AYUSH MISHRA <ayushm185@gmail.com>
2024-04-05 22:45:14 +0530

    updae

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"React-2-Project",
+  base:"/React-2-Project",
   plugins: [react()],
 })
